599986fb31bc7c52fe0393ac387dcb3da5930173
amirisback <faisalamircs@gmail.com>
2019-11-16 02:47:30 +0700

    update: Widget option

--- app/src/main/java/com/frogobox/mvvm/ui/widget/FavoriteBannerWidget.kt
+++ app/src/main/java/com/frogobox/mvvm/ui/widget/FavoriteBannerWidget.kt
@@ -1,5 +1,15 @@
 package com.frogobox.mvvm.ui.widget
 
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.widget.RemoteViews
+import android.widget.Toast
+import com.frogobox.mvvm.R
+
 /**
  * Created by Faisal Amir
  * FrogoBox Inc License
@@ -17,3 +27,72 @@ package com.frogobox.mvvm.ui.widget
  * com.frogobox.mvvm.ui.widget
  *
  */
+class FavoriteBannerWidget : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val mgr = AppWidgetManager.getInstance(context)
+        if (intent.action == TOAST_ACTION) {
+            val appWidgetId = intent.getIntExtra(
+                AppWidgetManager.EXTRA_APPWIDGET_ID,
+                AppWidgetManager.INVALID_APPWIDGET_ID
+            )
+            val viewIndex = intent.getIntExtra(EXTRA_ITEM, 0) + 1
+            Toast.makeText(context, "Favorite Movie Number : $viewIndex", Toast.LENGTH_SHORT).show()
+        }
+        super.onReceive(context, intent)
+    }
+
+    companion object {
+
+        val TOAST_ACTION = "toast_action"
+        val EXTRA_ITEM = "extra_movie"
+
+        internal fun updateAppWidget(
+            context: Context, appWidgetManager: AppWidgetManager,
+            appWidgetId: Int
+        ) {
+
+            val intent = Intent(context, StackWidgetService::class.java)
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+            intent.data = Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME))
+
+            val views = RemoteViews(context.packageName, R.layout.widget_banner_favorite)
+            views.setRemoteAdapter(R.id.stack_view, intent)
+            views.setEmptyView(R.id.stack_view, R.id.empty_view)
+
+            val toastIntent = Intent(context, FavoriteBannerWidget::class.java)
+            toastIntent.action = FavoriteBannerWidget.TOAST_ACTION
+            toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+
+            intent.data = Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME))
+            val toastPendingIntent = PendingIntent.getBroadcast(
+                context,
+                0,
+                toastIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+            views.setPendingIntentTemplate(R.id.stack_view, toastPendingIntent)
+
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
--- app/src/main/java/com/frogobox/mvvm/ui/widget/StackWidgetService.kt
+++ app/src/main/java/com/frogobox/mvvm/ui/widget/StackWidgetService.kt
@@ -1,5 +1,8 @@
 package com.frogobox.mvvm.ui.widget
 
+import android.content.Intent
+import android.widget.RemoteViewsService
+
 /**
  * Created by Faisal Amir
  * FrogoBox Inc License
@@ -17,3 +20,10 @@ package com.frogobox.mvvm.ui.widget
  * com.frogobox.mvvm.ui.widget
  *
  */
+class StackWidgetService : RemoteViewsService() {
+
+    override fun onGetViewFactory(intent: Intent): RemoteViewsFactory {
+        return StackRemoteViewFactory(this.applicationContext, intent)
+    }
+}
+
