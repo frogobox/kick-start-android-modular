c440aaef73849190a44a5b7965f9eca10aa0c4f4
amirisback <faisalamircs@gmail.com>
2019-11-16 02:44:37 +0700

    add: Widget Class Option

---
+++ app/src/main/java/com/frogobox/mvvm/ui/widget/StackRemoteViewFactory.kt
@@ -0,0 +1,19 @@
+package com.frogobox.mvvm.ui.widget
+
+/**
+ * Created by Faisal Amir
+ * FrogoBox Inc License
+ * =========================================
+ * mvvm
+ * Copyright (C) 16/11/2019.
+ * All rights reserved
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * LinkedIn : linkedin.com/in/faisalamircs
+ * -----------------------------------------
+ * FrogoBox Software Industries
+ * com.frogobox.mvvm.ui.widget
+ *
+ */
