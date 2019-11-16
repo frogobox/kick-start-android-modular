cae28396d8ff07e1801a4f459fa56d2809ca9641
amirisback <faisalamircs@gmail.com>
2019-11-16 10:11:11 +0700

    create: Activity Detail

---
+++ favorite/src/main/java/com/frogobox/favorite/ui/activity/DetailMovieActivity.kt
@@ -0,0 +1,11 @@
+package com.frogobox.favorite.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailMovieActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- favorite/src/main/java/com/frogobox/favorite/ui/fragment/TvShowFragment.kt
+++ favorite/src/main/java/com/frogobox/favorite/ui/fragment/TvShowFragment.kt
@@ -1,4 +1,4 @@
-package com.frogobox.favorite.fragment
+package com.frogobox.favorite.ui.fragment
 
 
 import android.os.Bundle
