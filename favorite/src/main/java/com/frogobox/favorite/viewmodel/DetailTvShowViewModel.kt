5f46c93865bf10b124570bb5ccc1f2256c4e86d5
amirisback <faisalamircs@gmail.com>
2019-11-16 10:08:56 +0700

    update: ViewModel Favorite

--- favorite/src/main/java/com/frogobox/favorite/viewmodel/DetailTvShowViewModel.kt
+++ favorite/src/main/java/com/frogobox/favorite/viewmodel/DetailTvShowViewModel.kt
@@ -1,5 +1,16 @@
 package com.frogobox.favorite.viewmodel
 
+import android.app.Application
+import android.database.Cursor
+import com.frogobox.base.BaseViewModel
+import com.frogobox.base.modular.callback.DeleteViewCallback
+import com.frogobox.base.modular.callback.SaveViewCallback
+import com.frogobox.base.modular.model.FavoriteTvShow
+import com.frogobox.base.modular.source.DataSource
+import com.frogobox.base.modular.source.Repository
+import com.frogobox.base.util.Constant.RoomDatabase.TvShow.setupCursor
+import com.frogobox.base.util.SingleLiveEvent
+
 /**
  * Created by Faisal Amir
  * FrogoBox Inc License
@@ -17,3 +28,74 @@ package com.frogobox.favorite.viewmodel
  * com.frogobox.favorite
  *
  */
+class DetailTvShowViewModel (private val context: Application, private val repository: Repository) :
+    BaseViewModel(context) {
+
+    var favoriteTvShow = SingleLiveEvent<FavoriteTvShow>()
+    var eventIsFavorite = SingleLiveEvent<Boolean>()
+
+    fun saveFavoriteTvShow(data: FavoriteTvShow, callback: SaveViewCallback){
+        callback.onShowProgress()
+        if (repository.saveFavoriteTvShow(context, data)){
+            callback.onHideProgress()
+            callback.onSuccesInsert()
+            eventIsFavorite.postValue(true)
+        } else {
+            callback.onHideProgress()
+            callback.onFailed("Failed")
+        }
+    }
+
+    fun deleteFavoriteTvShow(tableId: Int, callback: DeleteViewCallback){
+        callback.onShowProgress()
+        if (repository.deleteFavoriteMovieProvider(context, tableId)){
+            callback.onHideProgress()
+            callback.onSuccesDelete()
+            eventIsFavorite.postValue(false)
+        } else {
+            callback.onHideProgress()
+            callback.onFailed("Failed")
+        }
+    }
+
+    fun getFavoriteTvShow(id: Int){
+        repository.getFavoriteTvShowCursor(context, object : DataSource.GetLocalCallBack<Cursor>{
+            override fun onShowProgressDialog() {
+                eventShowProgress.postValue(true)
+            }
+
+            override fun onHideProgressDialog() {
+                eventShowProgress.postValue(false)
+            }
+
+            override fun onSuccess(data: Cursor) {
+
+                val tempFavoriteList = mutableListOf<FavoriteTvShow>()
+                tempFavoriteList.clear()
+                tempFavoriteList.addAll(setupCursor(data))
+
+                for (i in tempFavoriteList.indices) {
+                    if (tempFavoriteList[i].id!! == id) {
+                        eventIsEmpty.postValue(false)
+                        eventIsFavorite.postValue(true)
+                        favoriteTvShow.postValue(tempFavoriteList[i])
+                        break
+                    } else {
+                        eventIsEmpty.postValue(true)
+                        eventIsFavorite.postValue(false)
+                    }
+                }
+            }
+
+            override fun onFinish() {}
+
+            override fun onEmpty() {
+                eventIsEmpty.postValue(true)
+                eventIsFavorite.postValue(false)
+            }
+
+            override fun onFailed(statusCode: Int, errorMessage: String?) {}
+        })
+    }
+
+}
--- favorite/src/main/java/com/frogobox/favorite/viewmodel/TvShowViewModel.kt
+++ favorite/src/main/java/com/frogobox/favorite/viewmodel/TvShowViewModel.kt
@@ -1,5 +1,14 @@
 package com.frogobox.favorite.viewmodel
 
+import android.app.Application
+import android.database.Cursor
+import com.frogobox.base.BaseViewModel
+import com.frogobox.base.modular.model.FavoriteTvShow
+import com.frogobox.base.modular.source.DataSource
+import com.frogobox.base.modular.source.Repository
+import com.frogobox.base.util.Constant.RoomDatabase.TvShow.setupCursor
+import com.frogobox.base.util.SingleLiveEvent
+
 /**
  * Created by Faisal Amir
  * FrogoBox Inc License
@@ -17,3 +26,38 @@ package com.frogobox.favorite.viewmodel
  * com.frogobox.favorite
  *
  */
+class TvShowViewModel(private val context: Application, private val repository: Repository) :
+    BaseViewModel(context) {
+
+    var favTvShowListLive = SingleLiveEvent<List<FavoriteTvShow>>()
+
+    fun getFavoriteTvShow() {
+        repository.getFavoriteTvShowCursor(context, object : DataSource.GetLocalCallBack<Cursor> {
+            override fun onShowProgressDialog() {
+                eventShowProgress.postValue(true)
+            }
+
+            override fun onHideProgressDialog() {
+                eventShowProgress.postValue(false)
+            }
+
+            override fun onSuccess(data: Cursor) {
+                eventIsEmpty.postValue(false)
+                favTvShowListLive.postValue(setupCursor(data))
+            }
+
+            override fun onFinish() {
+
+            }
+
+            override fun onEmpty() {
+                eventIsEmpty.postValue(true)
+            }
+
+            override fun onFailed(statusCode: Int, errorMessage: String?) {
+
+            }
+        })
+    }
+
+}
