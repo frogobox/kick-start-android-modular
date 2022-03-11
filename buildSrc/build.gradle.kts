0aa7ca2bbed59387ad9e858992810fb3dcc1affa
Faisal Amir <faisalamircs@gmail.com>
2022-03-11 20:21:37 +0700

    Create build.gradle.kts

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,15 @@
+import org.gradle.kotlin.dsl.`kotlin-dsl`
+
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://jitpack.io") }
+}
+
+dependencies{
+    // library frogo-build-src
+    implementation("com.github.frogobox:open-build-src:1.0.1")
+}
